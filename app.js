1e11ed5bf1b9755ccdc9439cdd52283e0c6455bb
rouclec <senatorasonganyi97@gmail.com>
2023-01-26 12:00:32 +0100

    changed app.js

--- app.js
+++ app.js
@@ -24,18 +24,18 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 
 //use session to enable passing cookies between different domains
-app.use(
-  session({
-    resave: false,
-    saveUninitialized: false,
-    secret: "session",
-    cookie: {
-      maxAge: 1000 * 60 * 60,
-      sameSite: "none", //set to true if F.E. is on production
-      secure: false,
-    },
-  })
-);
+// app.use(
+//   session({
+//     resave: false,
+//     saveUninitialized: false,
+//     secret: "session",
+//     cookie: {
+//       maxAge: 1000 * 60 * 60,
+//       sameSite: "none", //set to true if F.E. is on production
+//       secure: false,
+//     },
+//   })
+// );
 app.use(express.json({ limit: "10kb" }));
 
 app.use("/api/v1/users", userRouter);
